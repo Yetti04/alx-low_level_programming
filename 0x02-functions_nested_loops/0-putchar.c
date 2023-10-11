ce1516d2d63d2f3db037edb924fd327682ceab9f
Yetti04 <quadriyetunde04@gmail.com>
2023-10-11 08:00:59 -0700

    program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
+
 /**
- * main - Print "Beauty" followed by a new line.
- * Description: You are not allowed to include standard libraries.
- * Return: 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int c = 0;
-char beauty[7] = "_putchar\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-while (c < 6)
-{
-_putchar(beauty[c]);
-c++;
-}
-return (0);
+	return (0);
 }
